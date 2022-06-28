3c64cb8ce5357526b8b6882083a542a233e723b4
QuanNguyen <quan.nguyen@investidea.tech>
2022-06-28 20:42:55 +0700

    update temp db

--- src/main/resources/db/migration/V1.1__test.sql
+++
@@ -1,8 +0,0 @@
-create table p_city
-(
-    id          bigserial
-        constraint p_city_pk primary key,
-    name        varchar(255),
-    province_id bigint not null,
-    path        varchar(255)
-);
